6039560af0fa36e7df71f3ccf5487f1ba409a829
heysungj <linjing0525@gmail.com>
2022-06-17 00:24:30 -0400

    added working aws to upload photos locally

--- controllers/ryokans.js
+++ controllers/ryokans.js
@@ -5,6 +5,14 @@ const Ryokan = require("../models/ryokan");
 const Review = require("../models/review");
 const User = require("../models/user");
 const express = require("express"); // import express
+const multer = require("multer");
+const AWS = require("aws-sdk"); //import AWS
+const fs = require("fs/promises");
+
+// Set the region
+AWS.config.update({ region: "us-east-1" });
+// Create S3 service object
+const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
 /////////////////////////////////////////
 // Create Route
@@ -76,6 +84,45 @@ router.get("/", async (req, res) => {
   }
 });
 
+// setup multer middleware to parse form-data
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, req.session.userId + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage, limits: { fieldSize: 5000 } });
+
+const readFile = async (file, urlArr) => {
+  try {
+    const fileResult = await fs.readFile("uploads/" + file);
+
+    // Buffer Pattern; how to handle buffers; straw, intake/outtake analogy
+    const base64data = new Buffer(fileResult, "binary");
+    try {
+      const result = await s3
+        .upload({
+          Bucket: "ryokan-photos",
+          Key: file,
+          Body: base64data,
+        })
+        .promise();
+      console.log(`File upload to S3 successfully at ${result.Location}`);
+      urlArr.push(result.Location);
+    } catch (e) {
+      console.log("Error uploading file to S3", err);
+    }
+  } catch (e) {
+    console.log("Error reading temp files", e);
+  }
+};
+
+const removeFile = async (file) => {
+  await fs.rm("uploads/" + file);
+};
+
 // create new ryokan route
 router.get("/new", (req, res) => {
   res.render("ryokans/new.liquid", {
@@ -84,9 +131,42 @@ router.get("/new", (req, res) => {
   });
 });
 
-router.post("/new", async (req, res) => {
-  await Ryokan.create(req.body);
-  res.redirect("/ryokans");
+router.post("/new", upload.array("photos"), async (req, res) => {
+  try {
+    // reg ex to match
+    const re = `${req.session.userId}`;
+    const regex = new RegExp(re);
+    const photoUrls = [];
+
+    const allFiles = await fs.readdir("uploads/");
+
+    const matches = allFiles.filter((filePath) => {
+      return filePath.match(regex);
+    });
+
+    const numFiles = matches.length;
+    if (numFiles) {
+      // Read in the file, convert it to base64, store to S3
+      for (i = 0; i < numFiles; i++) {
+        await readFile(matches[i], photoUrls);
+      }
+
+      for (i = 0; i < numFiles; i++) {
+        await removeFile(matches[i]);
+      }
+    }
+    console.log(photoUrls);
+
+    const newRyokan = new Ryokan(req.body);
+    newRyokan.img = photoUrls;
+    await newRyokan.save();
+
+    // redirect user to index page if successfully created item
+    res.redirect("/ryokans");
+  } catch (error) {
+    console.log("Error loading temp folder");
+    res.json({ error });
+  }
 });
 
 // show ryokan page
@@ -114,10 +194,39 @@ router.get("/:id", async (req, res) => {
   });
 });
 
-router.post("/:id/reviews/new", async (req, res) => {
+router.post("/:id/reviews/new", upload.array("photos"), async (req, res) => {
   const id = req.params.id;
   try {
-    const newReview = await Review.create(req.body);
+    // reg ex to match
+    const re = `${req.session.userId}`;
+    const regex = new RegExp(re);
+    const photoUrls = [];
+
+    const allFiles = await fs.readdir("uploads/");
+
+    const matches = allFiles.filter((filePath) => {
+      return filePath.match(regex);
+    });
+
+    const numFiles = matches.length;
+    if (numFiles) {
+      // Read in the file, convert it to base64, store to S3
+      for (i = 0; i < numFiles; i++) {
+        await readFile(matches[i], photoUrls);
+      }
+
+      for (i = 0; i < numFiles; i++) {
+        await removeFile(matches[i]);
+      }
+    }
+
+    console.log(photoUrls);
+
+    const newReview = new Review(req.body);
+    newReview.photo = photoUrls;
+    await newReview.save();
+    console.log(newReview);
+
     await Ryokan.findOneAndUpdate(
       { _id: id },
       {
@@ -125,11 +234,10 @@ router.post("/:id/reviews/new", async (req, res) => {
       }
     );
 
-    // console.log(newReview);
     // redirect user to index page if successfully created item
     res.redirect(`/ryokans/${id}`);
   } catch (error) {
-    console.log(error);
+    console.log("Error loading temp folder");
     res.json({ error });
   }
 });
